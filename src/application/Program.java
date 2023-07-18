b1d94e53c4686d929e9a77b96022ed8faa40bb80
zack junior <zacarias1992.12.19@gmail.com>
2023-07-18 15:43:06 -0300

    CheckMate logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -19,7 +19,7 @@ public class Program {
 		
 		
 		
-		while(true){
+		while(!chessMatch.getCheckMate()){
 			try {
 			UI.clearScreen();
 			UI.printMatch(chessMatch, captured);
@@ -50,5 +50,7 @@ public class Program {
 				sc.nextLine();
 			}
 		}
+		UI.clearScreen();
+		UI.printMatch(chessMatch, captured);
 	}
 }
